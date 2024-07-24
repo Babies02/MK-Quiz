4778c58dc3d347323583c7545a510df8c9322a3c
Babies02 <166697983+Babies02@users.noreply.github.com>
2024-07-24 23:13:31 +0200

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,138 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What color does yellow and red make?",
+    answer: "Orange",
+    options: [
+      "Blue",
+      "Orange",
+      "Pink",
+      "Brown"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What company produces the computer operating system Windows?",
+    answer: "Microsoft",
+    options: [
+      "Sony",
+      "IBM",
+      "Microsoft",
+      "Intel",
+    ]
+      
+  },
+    {
+    numb: 3,
+    question: "How many legs does a spider have?",
+    answer: "8",
+    options: [
+      "4",
+      "16",
+      "12",
+      "8",
+      
+    ]
+  },
+    {
+    numb: 4,
+    question: "Banana is a..?",
+    answer: "Fruit",
+    options: [
+      "Fruit",
+      "Nut",
+      "Vegetable",
+      "Protein"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which restaurant chain is the most visited in the world",
+    answer: "McDonalds",
+    options: [
+      "KFC",
+      "Burger King",
+      "McDonalds",
+      "Pizza Hut"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Which of these is a large animal that stays in the water a lot?",
+    answer: "Hippopotamus",
+    options: [
+      "Bald Eagle",
+      "Hippopotamus",
+      "Nightingale",
+      "Toad"
+    ]
+  },
+  {
+    numb: 7,
+    question: "What is largest mammal in the world?",
+    answer: "Blue whale",
+    options: [
+      "Sea lion",
+      "Bear",
+      "Blue whale",
+      "Elephant"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Who is the CEO of Facebook?",
+    answer: "Mark Zuckerberg",
+    options: [
+      "Elon Musk",
+      "Brad Pitt",
+      "Andrew Mcollum",
+      "Mark Zuckerberg"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Which of these has more than two eyes?",
+    answer: "Spider",
+    options: [
+      "Frog",
+      "Octopus",
+      "Giraffe",
+      "Spider"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Scorpions, Spiders, Mites, and ticks belong to what group/family",
+    answer: "Arachnids",
+    options: [
+      "Reptitles",
+      "Mammals",
+      "Protoza",
+      "Arachnids"
+    ]
+  },
+  {
+    numb: 11,
+    question: "Which animal's heart is inside its head?",
+    answer: "Shrimp",
+    options: [
+      "Monkey",
+      "Shrimp",
+      "Tiger",
+      "Dog"
+    ]
+  },
+  {
+    numb: 12,
+    question: "Which is not a fruit",
+    answer: "Potato",
+    options: [
+      "Orange",
+      "Grapes",
+      "Potato",
+      "Apple"
+    ]
+  },
+
+];
